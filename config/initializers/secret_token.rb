51a5b07e0d4e8dacea542225dff643d528826af4
jeevan <jeevan@jeevan-dell.(none)>
2012-08-11 14:08:19 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gypsy::Application.config.secret_token = '4cfeb58ed68f79a87db80b779a8672e41264b9abf5c8cf3cb3a6db801c861465ee8024d8ee052ac382d21661d19a9b2070cda0e36562c605f086503417580490'
